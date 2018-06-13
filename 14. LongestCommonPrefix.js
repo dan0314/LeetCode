6916e2f1cdaeaeba015c4af4919c0b0ff60dc6af
xiaosi <sisi101110@gmail.com>
2018-06-13 18:29:21 +0800

    14 一个智障现场

--- 14. LongestCommonPrefix.js
+++ 14. LongestCommonPrefix.js
@@ -13,8 +13,20 @@
  *
  */
 
-var longestCommonPrefix = function(strs) {
-    if(strs.length === 0 || strs[0].length === 0) return;
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0 || strs[0].length === 0) return;
+    let j = 0;
+    let prefix = '';
 
-    
+    for (j = 0; j < strs[0].length; j++) { 
+        for(let i = 0; i < strs.length - 1; i++){
+            if(strs[i][j] !== strs[i + 1][j]) return prefix;
+        }
+        prefix += strs[0][j];
+    }
+    return prefix;
 };
+console.log(
+    longestCommonPrefix(["xiaosi", "xiaosi2", "xiaosi46", "xiaosdgfg"])
+);
+console.log(longestCommonPrefix(["xiaoddsi", "cvfdb", "qwe", "hhgjhg"]));
